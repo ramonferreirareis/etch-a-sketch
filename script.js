8cb332b2c96044f4460cfbebdf051d002f7aa068
Ramon Reis <111209853+ramonferreirareis@users.noreply.github.com>
2024-11-12 20:03:26 -0300

    Change function to create rows with a number of divs

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const canvas = document.querySelector("#canvas")
 
-function createSquare(number) {
-    for (i = 0; i < number; i++) {
-        const squares = document.createElement("div");
-        squares.classList.add("square");
-        canvas.appendChild(squares);
-    }
+function createGrid(number) {
+    for (let i = 0; i < number; i++) {
+        const divRow = document.createElement("div");
+        divRow.classList.add("row");
+        canvas.appendChild(divRow);
+        for (let i = 0; i < number; i++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            divRow.appendChild(square);
+        }
+    }  
 }
-
+createGrid(5);
