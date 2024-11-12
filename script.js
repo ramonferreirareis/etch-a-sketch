d8bde5cb37d636337d79b4d74ff093b646e05f4a
Ramon Reis <111209853+ramonferreirareis@users.noreply.github.com>
2024-11-12 20:50:13 -0300

    Add event to change color of squares when mouse hovers on it

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const canvas = document.querySelector("#canvas")
 
+
+
 function createGrid(number) {
     for (let i = 0; i < number; i++) {
         const divRow = document.createElement("div");
@@ -8,7 +10,11 @@ function createGrid(number) {
         for (let i = 0; i < number; i++) {
             const square = document.createElement("div");
             square.classList.add("square");
+            square.addEventListener("mouseenter", () => {
+                square.classList.add("color");
+            });
             divRow.appendChild(square);
+            
         }
     }  
 }
